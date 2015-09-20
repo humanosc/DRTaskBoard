b6beba5bab7a9097daa2a94b16738a2112a42521
humanosc <humanosc@gmx.de>
2015-09-20 23:55:45 +0200

    - added taskboard service for data access - added taskboard database - added taskboard database entity model

---
+++ DRTaskBoardTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DRTaskBoardTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
